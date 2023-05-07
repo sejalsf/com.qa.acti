54a93b1673ed219e4337a0b8aaa2764df21d5137
Sejal Panchal <Sejal Panchal@DESKTOP-CJN3ODS>
2023-05-07 14:23:13 -0500

    added author details to enter page

--- src/main/java/com/acti/pages/Enterpage.java
+++ src/main/java/com/acti/pages/Enterpage.java
@@ -4,4 +4,12 @@ import com.acti.base.DriverScript;
 
 public class Enterpage extends DriverScript{
 
+/*
+ * Name : Driverscript
+ * Description - Manges the driver and borwser related configration
+ * Developed by - Sejal
+ * Reviewed - Shantosh
+ * Date reviewed - 04/27/2023
+ */
+
 }
