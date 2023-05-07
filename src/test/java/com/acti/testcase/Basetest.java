83772452e416356e899dae9bd7ce24de9a8199f2
Sejal Panchal <Sejal Panchal@DESKTOP-CJN3ODS>
2023-05-07 15:11:33 -0500

    Added new code to the system

--- src/test/java/com/acti/testcase/Basetest.java
+++ src/test/java/com/acti/testcase/Basetest.java
@@ -5,18 +5,19 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.acti.base.DriverScript;
+import com.acti.pages.Enterpage;
 import com.acti.pages.LoginPage;
 
 public class Basetest extends DriverScript {
 
 	LoginPage lp;
-	EnterPage EC;
+	Enterpage EC;
 
 	@BeforeMethod
 	public void pretest() {
 		initApplication();// lauch the browser
 		lp = new LoginPage();/// Create an object of new login page
-        EC = new EnterPage();
+        EC = new Enterpage();
 	}
 
 	@AfterMethod
