e5b2e902c5c9a9754c1e1260eef1c45793e1999f
Sejal Panchal <Sejal Panchal@DESKTOP-CJN3ODS>
2023-06-29 14:44:35 -0500

    Add thread sleep in main code

--- src/main/java/com/acti/utils/Helper.java
+++ src/main/java/com/acti/utils/Helper.java
@@ -3,6 +3,16 @@ package com.acti.utils;
 import com.acti.base.DriverScript;
 
 public class Helper extends DriverScript{
+	public void sleep() {
+		
+		try {
+			Thread.sleep(3000);
+		} 
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
 
 }
